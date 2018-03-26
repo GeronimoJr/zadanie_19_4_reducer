75638f09c69c64215627f1560df8686ad4ac0d40
Arnold Sztandarski <sztandarski.arnold@gmail.com>
2018-03-26 18:10:13 +0200

    Add files

---
+++ actions.js
@@ -0,0 +1,44 @@
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+};
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+};
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+};
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id
+    }
+};
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
+    }
+};
+
+const boundAddComment = text => dispatch(addComment(text));
+const boundEditComment = (id, text) => dispatch(editComment(id, text));
+const boundRemoveComment = id => dispatch(removeComment(id));
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
