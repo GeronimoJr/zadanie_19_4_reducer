72a8505ddc1214f49de649ded2ea2d262e452fbe
Arnold Sztandarski <sztandarski.arnold@gmail.com>
2018-03-27 20:45:03 +0200

    Change forEach to map

--- comments.js
+++ comments.js
@@ -8,43 +8,40 @@ import {THUMB_DOWN_COMMENT} from './actions';
 function comments(state = [], action) {
     switch(action.type) {
         case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                    {
+            return [{
                         id: action.id,
                         text: action.text,
                         votes: 0
                     }
-                    , ...state.comments]
-            });
+                    , ...state.comments];
         case REMOVE_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.filter(comment => comment.id == action.id)
-        });
+        return [{
+            comments: state.comments.filter(comment => comment.id !== action.id)
+        }];
         case EDIT_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.forEach((comment, action) => {
+        return [{
+            comments: state.comments.map(comment => {
                 if (comment.id === action.id) {
                     comment.text = action.text;
                 }
             })
-        });
+        }];
         case THUMB_UP_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.forEach((comment, action) => {
+        return [{
+            comments: state.comments.map(comment => {
                 if (comment.id === action.id) {
                     comment.votes + 1;
                 }
             })
-        });
+        }];
         case THUMB_DOWN_COMMENT:
-        return Object.assign({}, state, {
-            comments: state.comments.forEach((comment, action) => {
+        return [{
+            comments: state.comments.map(comment => {
                 if (comment.id === action.id) {
                     comment.votes + 1;
                 }
             })
-        });
+        }];
         default:
             return state.comments;
     }
